40c36a0679c94f83c4c366a490648ebede246c08
Ricardo Godoy <ricardofagodoy@gmail.com>
2015-12-21 03:09:17 -0600

    Fixed some bugs and improved css screen fit

--- platforms/android/assets/www/scripts/app.js
+++ platforms/android/assets/www/scripts/app.js
@@ -118,24 +118,29 @@ var Menu = {
             instructions: $('#menu #options #instructions'),
             status: $('#menu #options #status'),
             credits: $('#menu #options #credits'),
-            creditsScreen: $('#credits')
+            creditsScreen: $('#creditsScreen'),
+            creditsScreenBack: $('#creditsScreen #thanks')
         };
         
-        // If menu's option is NEW GAME or CONTINUE
-        if(level > 1)
-            this.dom.play.html(messages['menu.continue']);
-    
         // If he's already won, different layout
         if(level > properties.maxLevel)
             this.configureWinner();
         else
             this.configureOriginal();
         
+        // If menu's option is NEW GAME or CONTINUE
+        if(level > 1 && level <= properties.maxLevel)
+            this.dom.play.html(messages['menu.continue']);
+        
         // Click on credits
         this.dom.credits.off('click').on('click', function(event) {
             Menu.dom.self.fadeOut("fast", function() {
-                Maze.dom.creditsScreen.fadeIn("fast");
+                Menu.dom.creditsScreen.fadeIn("fast");
             });
+        });
+        
+        this.dom.creditsScreenBack.off('click').on('click', function(event) {
+            onBackButtonPress();
         });
     },
     
@@ -903,7 +908,13 @@ function onBackButtonPress() {
     if(Card.status == STATUS.NEXT || Card.status == STATUS.LOSE)
         return;
     
-    if (Maze.dom.self.is(':visible')) {
+    if (Menu.dom.creditsScreen.is(':visible')) {
+        
+        Menu.dom.creditsScreen.fadeOut("fast", function() {
+            Menu.dom.self.fadeIn("fast");
+        });
+        
+    } else if (Maze.dom.self.is(':visible')) {
         Maze.dom.self.fadeOut("fast", function() {
 
             // Check if change New game to Continue
